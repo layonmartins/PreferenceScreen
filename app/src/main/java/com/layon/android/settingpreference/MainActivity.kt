b4f2f9a2007e95bfbf04d0c144ab3c02b2fc79f4
Layon Martins <layonmartins03@gmail.com>
2020-07-31 15:47:25 -0300

    create the menu preference option

--- app/src/main/java/com/layon/android/settingpreference/MainActivity.kt
+++ app/src/main/java/com/layon/android/settingpreference/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.layon.android.settingpreference
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+
+        if(id == R.id.item_preference)
+        {
+            goToPreferenceActivity();
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun goToPreferenceActivity() {
+        startActivity(Intent(this, PreferenceActivity::class.java))
     }
 }
